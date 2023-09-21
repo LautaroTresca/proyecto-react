d1cfdfa5556f6eb7cb8668eb747d0dac187fe297
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-09-21 17:10:06 -0300

    pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,6 @@ import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const Boton = ({parametro}) => {
-    return(
-        <button>{parametro}</button>
-    )
-}
-
 
 root.render(
     <React.Fragment>
